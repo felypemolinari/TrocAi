63ce9f3e1ac620168cddf89dbe60bb94b5097f4a
Felype Molinari <felype.molinari@gmail.com>
2025-10-14 22:50:09 -0300

    ajustes de uns parametros para puxar o nome do user

--- Models/Service.js
+++ Models/Service.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema({
-  servico: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  descricao: {
-    type: String,
-    required: true
-  },
-  usuario: {
-    type: String,
-    required: true
-  }
+    servico: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    },
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // 'User' é o nome do seu modelo
+        required: true
+    }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 const Service = mongoose.model('Service', serviceSchema);
